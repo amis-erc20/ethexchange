a58bb2b95bdbc446dffdfe2de3a1ebd9814eb8b2
amisolution <siriusgem1@gmail.com>
2018-08-22 04:22:55 +0300

    Update start.php

--- start.php
+++ start.php
@@ -12,9 +12,9 @@
         <link rel="apple-touch-icon" href="icon.png">
         <!-- Place favicon.ico in the root directory -->
 
-        <link rel="stylesheet" href="css/vendor/bootstrap.min.css">
-        <link rel="stylesheet" href="css/normalize.css">
-        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="/ethexchange/css/vendor/bootstrap.min.css">
+        <link rel="stylesheet" href="/ethexchange/css/normalize.css">
+        <link rel="stylesheet" href="/ethexchange/css/main.css">
     </head>
     <body>
         <!--[if lte IE 9]>
@@ -34,7 +34,7 @@
                                 <td class="menu-bar-col-1">
                                     <button type="button" class="btn btn-primary clearfix">
                                     <div class="button-text">
-                                        GJC
+                                        AMIS
                                     </div> 
                                     <div class="triangle-down"></div>
                                     </button>
@@ -60,7 +60,7 @@
                                 <div class="exchange-balance">
                                     <h3>Exchange Balance</h3>
                                     <div class="row">
-                                        <div class="col"><div class="btn btn-success balance" role="alert"><span class="badge badge-light">GJC</span> <span class="number">0.06972</span></div></div>
+                                        <div class="col"><div class="btn btn-success balance" role="alert"><span class="badge badge-light">AMIS</span> <span class="number">0.06972</span></div></div>
                                         <div class="col"><div class="btn btn-success balance" role="alert"><span class="badge badge-light">ETH</span> <span class="number">1.23456</span></div></div>
                                     </div>
                                 </div>
@@ -68,7 +68,7 @@
                                 <div class="wallet-balance">
                                     <h3>Wallet Balance</h3>
                                     <div class="row">
-                                        <div class="col"><div class="btn btn-success btn-sm balance" role="alert"><span class="badge badge-light">GJC</span> <span class="number">0.06972</span></div></div>
+                                        <div class="col"><div class="btn btn-success btn-sm balance" role="alert"><span class="badge badge-light">AMIS</span> <span class="number">0.06972</span></div></div>
                                         <div class="col"><div class="btn btn-success btn-sm balance" role="alert"><span class="badge badge-light">ETH</span> <span class="number">1.23456</span></div></div>
                                     </div>
                                 </div>
@@ -90,11 +90,11 @@
                                     <div class="clearfix">
                                         <div class="input-group mb-3">
                                             <div class="input-group-prepend">
-                                                <span class="input-group-text" id="basic-addon1">GJC</span>
+                                                <span class="input-group-text" id="basic-addon1">AMIS</span>
                                             </div>
                                             <input type="text" class="form-control" placeholder="Amount" aria-label="Amount" aria-describedby="basic-addon1">
                                         </div>
-                                        <button class="btn btn-primary" type="button">Deposit <span class="badge badge-light">GJC</span></button>
+                                        <button class="btn btn-primary" type="button">Deposit <span class="badge badge-light">AMIS</span></button>
                                     </div> 
                                 </div>
                             </div>
@@ -115,11 +115,11 @@
                                     <div class="clearfix">
                                         <div class="input-group mb-3">
                                             <div class="input-group-prepend">
-                                                <span class="input-group-text" id="basic-addon1">GJC</span>
+                                                <span class="input-group-text" id="basic-addon1">AMIS</span>
                                             </div>
                                             <input type="text" class="form-control" placeholder="Amount" aria-label="Amount" aria-describedby="basic-addon1">
                                         </div>
-                                        <button class="btn btn-primary" type="button">Widthdraw <span class="badge badge-light">GJC</span></button>
+                                        <button class="btn btn-primary" type="button">Widthdraw <span class="badge badge-light">AMIS</span></button>
                                     </div> 
                                 </div>
                             </div>
@@ -144,12 +144,12 @@
                                     <div class="clearfix">
                                         <div class="input-group mb-3">
                                             <div class="input-group-prepend">
-                                                <span class="input-group-text" id="basic-addon1">GJC</span>
+                                                <span class="input-group-text" id="basic-addon1">AMIS</span>
                                             </div>
                                             <input type="text" class="form-control" placeholder="Address" aria-label="Address" aria-describedby="basic-addon1">
                                             <input type="text" class="form-control" placeholder="Amount" aria-label="Amount" aria-describedby="basic-addon1">
                                             <div class="input-group-append">
-                                                <button class="btn btn-primary" type="button">Transfer <span class="badge badge-light">GJC</span></button>
+                                                <button class="btn btn-primary" type="button">Transfer <span class="badge badge-light">AMIS</span></button>
                                             </div>
                                         </div>
                                     </div> 
@@ -171,18 +171,18 @@
 
         </div>
 
-        <script src="js/vendor/modernizr-3.5.0.min.js"></script>
+        <script src="/ethexchange/js/vendor/modernizr-3.5.0.min.js"></script>
         <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.2.1.min.js"><\/script>')</script>
-        <script src="js/vendor/bootstrap.bundle.min.js"></script>
-        <script src="js/vendor/bootstrap.min.js"></script>
-        <script src="js/plugins.js"></script>
-        <script src="js/main.js"></script>
+        <script src="/ethexchange/js/vendor/bootstrap.bundle.min.js"></script>
+        <script src="/ethexchange/js/vendor/bootstrap.min.js"></script>
+        <script src="/ethexchange/js/plugins.js"></script>
+        <script src="/ethexchange/js/main.js"></script>
 
         <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
         <script>
             window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;
-            ga('create','UA-XXXXX-Y','auto');ga('send','pageview')
+            ga('create','UA--Y','auto');ga('send','pageview')
         </script>
         <script src="https://www.google-analytics.com/analytics.js" async defer></script>
     </body>
